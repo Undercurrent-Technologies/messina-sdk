6974c10d1329c5136c340e839b69ee9b8c988767
Laelnaldi Saputra <laelnaldisaputraa@gmail.com>
2022-11-30 18:16:09 +0700

    Update code examples

--- docs/examples/src/algo_to_eth.ts
+++ docs/examples/src/algo_to_eth.ts
@@ -1,114 +1,31 @@
 import { ethers } from "ethers";
 import algosdk, { Account, Algodv2, assignGroupID, waitForConfirmation } from "algosdk";
-import { redeemOnEth, transferFromAlgorand, parseSequenceFromLogAlgorand, getEmitterAddressAlgorand, nativeToHexString } from "@algo-foundry/messina-sdk";
-import { getSignedVAA } from "@algo-foundry/messina-sdk/lib/cjs/rpc/getSignedVAAUpdate";
+import { transferFromAlgorand, nativeToHexString, CHAIN_ID_ETH, getEmitterAddressAlgorand, parseSequenceFromLogAlgorand, redeemOnEth } from "@algo-foundry/messina-sdk";
 import { TransactionSignerPair } from "@algo-foundry/messina-sdk/lib/cjs/algorand";
+import { getSignedVAA } from "@algo-foundry/messina-sdk/lib/cjs/rpc/getSignedVAAUpdate";
 
-// algo node token
+//algorand
 const ALGO_TOKEN = "";
-// algo node host
-const ALGOD_ADDRESS = "";
-// algo node port
-const ALGOD_PORT = 0;
-// algo account mnemonic
-const ALGO_MNEMONIC_TEST = "";
+const ALGO_ADDRESS = "";
+const ALGO_PORT = "";
+const ALGO_MNEMONIC = "";
 
-// algo contracts
-const CORE_ID = 0;
-const TOKEN_BRIDGE_ID = 0;
+//contract
+const ALGO_BRIDGE_ID = 0;
+const ALGO_CORE_ID = 0;
+const ETH_BRIDGE_ADRESS = "";
 
-// eth rpc
+//ethereum
 const ETH_NODE_URL = "";
-// eth chain id
-const CHAIN_ID_ETH = 2;
-// eth private key
 const ETH_PRIVATE_KEY = "";
-// eth token bridge
-const TOKEN_BRIDGE_ADDRESS = "";
 
-// guardian host
+//guardian
 const GUARDIAN_GRPC = "";
-// algo asset details
+
+//algorand asset details
 const ASSET_ID = 0;
-const ASSET_DECIMAL = 0;
-// amount to transfer
+const ASSET_DECIMALS = 0;
 const AMOUNT_TO_TRANSFER = 0;
-
-async function main(): Promise<void> {
-  try {
-    console.log("Create algo client")
-    const client: algosdk.Algodv2 = getAlgoClient();
-    console.log("Create algo wallet");
-    const wallet: Account = algosdk.mnemonicToSecretKey(ALGO_MNEMONIC_TEST)
-
-    console.log("Create eth provider")
-    const provider = new ethers.providers.WebSocketProvider(
-      ETH_NODE_URL
-    ) as any;
-    console.log("Create eth wallet")
-    const signer = new ethers.Wallet(ETH_PRIVATE_KEY, provider);
-
-    console.log("Getting emitter address...");
-    const emitterAddr = getEmitterAddressAlgorand(BigInt(TOKEN_BRIDGE_ID));
-
-    console.log("Start transfer from Algorand to Ethereum")
-    const hexStr = nativeToHexString(
-      signer.address,
-      CHAIN_ID_ETH
-    );
-
-    if (!hexStr) {
-      throw new Error("Failed to convert to hexStr");
-    }
-
-    console.log("BRIDGING ALGO -> ETH: transferFromAlgorand");
-    const transferTxs = await transferFromAlgorand(
-      client,
-      BigInt(TOKEN_BRIDGE_ID),
-      BigInt(CORE_ID),
-      wallet.addr,
-      BigInt(ASSET_ID),
-      BigInt(AMOUNT_TO_TRANSFER * Math.pow(10, ASSET_DECIMAL)),
-      hexStr,
-      CHAIN_ID_ETH,
-      BigInt(0)
-    );
-
-    console.log("BRIDGING ALGO -> ETH: signAlgorandTransaction")
-    const transferResult = await signSendAndConfirmAlgorand(
-      client,
-      transferTxs,
-      wallet,
-      4
-    );
-
-    console.log("BRIDGING ALGO -> ETH: getSequenceNumber")
-    const txSid = parseSequenceFromLogAlgorand(transferResult);
-
-    console.log("BRIDGING ALGO -> ETH: getSignedVAA")
-    let signedVAA = await getSignedVAA(GUARDIAN_GRPC, emitterAddr, 'CHAIN_ID_ALGORAND', txSid, 1000, 5)
-
-    console.log("BRIDGING ALGO -> ETH: parseSignedVaa")
-    // @ts-ignore
-    const vaaBytes = new Uint8Array(signedVAA!.match(/.{1,2}/g)!.map((byte: any) => parseInt(byte, 16)))
-
-    console.log("BRIDGING ALGO -> ETH: redeemOnEth to receive the asset")
-    const redeem = await redeemOnEth(
-      TOKEN_BRIDGE_ADDRESS,
-      signer,
-      vaaBytes
-    );
-
-    console.log("BRIDGING ALGO -> ETH: success => " + redeem.transactionHash)
-  } catch (e) {
-    console.log("ERROR => " + e);
-  }
-}
-
-function getAlgoClient(): Algodv2 {
-  const algodClient = new Algodv2(ALGO_TOKEN, ALGOD_ADDRESS, ALGOD_PORT);
-  return algodClient;
-}
 
 async function signSendAndConfirmAlgorand(
   algodClient: Algodv2,
@@ -133,6 +50,71 @@ async function signSendAndConfirmAlgorand(
     waitRounds
   );
   return result;
+}
+
+async function main(): Promise<void> {
+  try{
+    console.log("INITIATE ALGO CLIENT");  
+    const client: algosdk.Algodv2 = new Algodv2(ALGO_TOKEN, ALGO_ADDRESS, ALGO_PORT);
+    const wallet: Account = algosdk.mnemonicToSecretKey(ALGO_MNEMONIC)
+    
+    console.log("INITIATE ETHERS CLIENT");
+    const provider = new ethers.providers.WebSocketProvider(ETH_NODE_URL) as any;   
+    const signer = new ethers.Wallet(ETH_PRIVATE_KEY, provider);
+    
+    const hexStr = nativeToHexString(
+          signer.address,
+          CHAIN_ID_ETH
+        );
+    
+    if (!hexStr) {
+       throw new Error("Failed to convert to hexStr");
+     }    
+   
+    console.log("TRANSFER FROM ALGORAND");  
+    const transferTxs = await transferFromAlgorand(
+          client,
+          BigInt(ALGO_BRIDGE_ID),
+          BigInt(ALGO_CORE_ID),
+          wallet.addr,
+          BigInt(ASSET_ID),
+          BigInt(AMOUNT_TO_TRANSFER * Math.pow(10, ASSET_DECIMALS)), // AMOUNT_TO_TRANSFER * (10 ^ ASSET_DECIMALS)
+          hexStr,
+          CHAIN_ID_ETH,
+          BigInt(0)
+        );
+    
+    console.log("SIGN SEND CONFIRM");    
+    const transferResult = await signSendAndConfirmAlgorand(
+          client,
+          transferTxs,
+          wallet,
+          4
+        );
+    
+    console.log("GET EMITTER"); 
+    const emitterAddr = getEmitterAddressAlgorand(BigInt(ALGO_BRIDGE_ID));
+
+    console.log("PARSE SEQUENCE"); 
+    const sequence = await parseSequenceFromLogAlgorand(transferResult);
+    
+    console.log("SIGNED VAA"); 
+    let signedVAA = await getSignedVAA(GUARDIAN_GRPC, emitterAddr, 'CHAIN_ID_ALGORAND', sequence, 1000, 5);
+    // @ts-ignore
+    const vaaBytes = new Uint8Array(signedVAA!.match(/.{1,2}/g)!.map((byte: any) => parseInt(byte, 16)));
+    
+    console.log("REDEEM ETH");  
+    const redeem = await redeemOnEth(
+          ETH_BRIDGE_ADRESS,
+          signer, // Wallet from ethers js
+          vaaBytes
+        );
+
+    console.log("SUCCESSFULLY REDEEM =>" +redeem);  
+        
+  }catch(e){
+    console.log("ERROR => " + e);
+  }
 }
 
 main();
